8b634292d2f7d748031b216cc214e1a829937896
Stark-X <xiaojiezhi2@gmail.com>
2023-03-26 19:38:09 +0800

    feat: use rich module to prettify, optimize the prompt

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import logging
-import sys
-
-import chromadb
-from llama_index import GPTChromaIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain import OpenAI
 from pathlib import Path
-from reader.markdown import MakrdownReader
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger()
-logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+from langchain.chat_models import ChatOpenAI
+from llama_index import (
+    GPTSimpleVectorIndex,
+    LLMPredictor,
+    PromptHelper,
+    SimpleDirectoryReader,
+)
+from llama_index import QuestionAnswerPrompt
+from rich.console import Console
+
 
 # define LLM
-llm_predictor = LLMPredictor(OpenAI(
-    temperature=0, model_name="gpt-3.5-turbo",  client=None))
+llm_predictor = LLMPredictor(ChatOpenAI(temperature=0,  client=None))
 
 # define prompt helper
 # set maximum input size
@@ -25,23 +24,34 @@ max_chunk_overlap = 20
 prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
 
 
-# in-memory store
-chroma_client = chromadb.Client()
-chroma_collection = chroma_client.get_or_create_collection("test")
-
-reader = MakrdownReader("data")
+reader = SimpleDirectoryReader(input_files=["data.txt"])
 docs = reader.load_data()
 
+QA_PROMPT_TMPL = (
+    "Providing you context info below.\n"
+    "---------------------\n"
+    "{context_str}"
+    "---------------------\n"
+    "Given this info, please answer the question with refer url: {query_str}\n"
+)
+QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
 #  index = GPTChromaIndex(docs, chroma_collection)
 if (index_store := Path("index.json")).exists():
-    index = GPTSimpleVectorIndex.load_from_disk(index_store.as_posix())
+    index = GPTSimpleVectorIndex.load_from_disk(index_store.as_posix(
+    ), llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 else:
     index = GPTSimpleVectorIndex(
         docs, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
     index.save_to_disk(index_store.as_posix())
 
+
+console = Console()
+
+count = 1
 while True:
-    quiz = input("Quiestion:")
-    response = index.query(quiz)
-    print(response)
-    print(f"Token usage: {index.llm_predictor.last_token_usage}")
+    console.rule(f"Quiz [{count}]")
+    quiz = console.input("[blue underline]Quiestion: ")
+    with console.status("Querying...", spinner="dots3"):
+        response = index.query(quiz, text_qa_template=QA_PROMPT)
+        console.print(f"[green]{response}")
+    count += 1
